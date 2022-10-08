9dfc92a3c3ddd021df51d425c0cb5bb951ba51b1
andartelolat <96941993+andartelolat@users.noreply.github.com>
2022-10-07 20:07:56 -0600

    Update get1iben.h

--- get1iben.h
+++ get1iben.h
@@ -1,18 +1,19 @@
 // File header Library
 
-// Fungsi Library
-#ifndef get1iben_H
-#define get1iben_H
-#include <Arduino.h>
+// Fungsi Library bungkus
+  #ifndef get1iben_H
+  #define get1iben_H
+  #include <Arduino.h>
+// tutup bungkus
 
-// membuat class
-class get1iben
-{
+// Class iben
+class get1iben{
   public:
   get1iben();
   ~get1iben();
   int baca_adc(int channel);
   void baca_data(void);
   void print_data(void);
+  void pesanBaru(int nmrPesan);
 };
 #endif
